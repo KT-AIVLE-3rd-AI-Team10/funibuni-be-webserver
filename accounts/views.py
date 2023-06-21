63b74a22fa16380d55fb538801d0845a79ca88a6
어남준 <aun4654@naver.com>
2023-06-22 01:02:23 +0900

    회원정보

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import AccessToken
 from accounts.models import BlacklistedToken,OutstandingToken
 from accounts.auth.authentications import JWTAuthenticationForRefresh
+from django.db import IntegrityError
 
 #회원가입
 @api_view(['POST'])
@@ -22,8 +23,8 @@ def user_signup_view(request):
         access = refresh.access_token
 
         return Response({
-            'refresh': str(refresh),
-            'access': str(access),
+            'access_token': str(access),
+            'refresh_token': str(refresh)
         }, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -39,6 +40,8 @@ def phone_number_login(request):
         user = User.objects.get(phone_number=phone_number)
     except User.DoesNotExist:
         return Response({'error': 'User not found'}, status=400)
+    except IntegrityError:
+            return Response({'error': 'Token already blacklisted'}, status=status.HTTP_400_BAD_REQUEST)
 
     refresh = RefreshToken.for_user(user)
 
@@ -58,15 +61,19 @@ def auto_signin(request):
         # RefreshToken을 사용하여 토큰 유효성 검증
         try:
             refresh = RefreshToken(refresh_token)
-            user = refresh.get('user')
             access_token = refresh.access_token
             BlacklistedToken.objects.create(token=str(refresh_token))
+            new_refresh = RefreshToken.for_user(refresh.get_user())
+            new_refresh_token = new_refresh.access_token
             return Response({'access_token': str(access_token),
-                             'refresh_token': str(refresh_token)}, status=status.HTTP_200_OK)
+                             'refresh_token': str(new_refresh_token)}, status=status.HTTP_200_OK)
         except TokenError as e:
             return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        except IntegrityError:
+            return Response({'error': 'Token already blacklisted'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error':  'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+
     
 #access토큰 재발급
 @api_view(['POST'])
@@ -83,8 +90,11 @@ def token_refresh(request):
             return Response({'access_token': access_token}, status=200)
         except TokenError as e:
             return Response({'error': str(e)}, status=401)
+        except IntegrityError:
+            return Response({'error': 'Token already blacklisted'}, status=status.HTTP_400_BAD_REQUEST)
     else:
         return Response({'error': 'Refresh token is required'}, status=400)
+    
 #로그아웃 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -109,6 +119,8 @@ def user_logout_view(request):
             return Response({'message': '로그아웃되었습니다.'}, status=status.HTTP_200_OK)
         except TokenError as e:
             return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        except IntegrityError:
+            return Response({'error': 'Token already blacklisted'}, status=status.HTTP_400_BAD_REQUEST)
     else:
         return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
     
@@ -137,6 +149,17 @@ def user_info_view(request):
 @permission_classes([IsAuthenticated])
 def user_delete_view(request):
     user = request.user
+    refresh_token = request.headers.get('Authorization')
+
+    if refresh_token:
+        refresh_token = refresh_token.replace('Bearer ', '')
+        try:
+            refresh = RefreshToken(refresh_token)
+            BlacklistedToken.objects.create(token=str(refresh_token))
+        except TokenError as e:
+            return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        except IntegrityError:
+            return Response({'error': '토큰이 이미 블랙리스트에 등록되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
     user.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
-
